8dee336cc57d2865bffd61f7582c5ceb0d4b88d2
Brett Bass <bhbass@proton.me>
2024-11-24 18:35:51 -0800

    converted from fork to pthreads

--- src/sensor_simulator.c
+++ src/sensor_simulator.c
@@ -1,3 +1,4 @@
+#include <pthread.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
@@ -8,64 +9,50 @@
 #include <sys/types.h>
 #include <string.h>
 
-//#include <signal.h>
-//#include <errno.h>
 
 #include "../include/sensor_simulator.h"
-
-
-// buffered sensor data, initalized to 0 for disabled sensors
-//float sensor_buffer [NUM_SENSORS] = { 0 };
 
 
 float generate_sensor_data() {
     return ((float)rand() / (float)(RAND_MAX)) * 100.0f;
 }
 
-void read_sensors(uint8_t sensors_enabled, shared_memory_t* shared_memory){
+void read_sensors(uint8_t sensors_enabled){
     float data;
 
     for (int i = 0; i < NUM_SENSORS; i++) {
         // check if current sensor is enabled
         if (sensors_enabled & (1<<i)) {
             data = generate_sensor_data();
-            shared_memory->sensor_buffer[i] = data;
+            shared_memory.sensor_buffer[i] = data;
         }
     }
 }
 
-void simulate_sensor_data(uint8_t sensors_enabled, shared_memory_t* shared_memory, int sampling_rate_ms) {
+void *simulate_sensor_data(void* arg) {
+    simulation_params_t *params = (simulation_params_t *)arg;
+    uint8_t sensors_enabled = params->sensors_enabled;
+    int sampling_rate_ms = params->sampling_rate_ms;
     srand(time(NULL));
     while (1) {
-        //pthread_mutex_lock(&shared_memory->mutex);
-        while(pthread_mutex_lock(&shared_memory->mutex) != 0);
+        pthread_mutex_lock(&shared_memory.mutex);
 
         // critical zone
-        if(shared_memory->interrupt_sensors) {
-            pthread_mutex_unlock(&shared_memory->mutex);
+        if(shared_memory.interrupt_sensors) {
+            pthread_mutex_unlock(&shared_memory.mutex);
             break;
         }
 
-        read_sensors(sensors_enabled, shared_memory);
-        shared_memory->sensors_updated = 1;
+        read_sensors(sensors_enabled);
+        shared_memory.sensors_updated = 1;
 
         // end critical zone
-        pthread_mutex_unlock(&shared_memory->mutex);
+        pthread_mutex_unlock(&shared_memory.mutex);
 
         // sleep to maintain sampling rate
         usleep(1000*sampling_rate_ms);
     }
 
-    printf("Dying child\n");
-
-    // Close pipe
-    //if (close(p) == -1) {
-    //    // Print error with line number
-    //    fprintf(stderr, "Error closing pipe at line %d: %s\n", __LINE__, strerror(errno));
-    //    exit(EXIT_FAILURE);
-    //}
-    printf("Child process terminating gracefully...\n");
-
-    //kill(getpid(), SIGTERM);
+    pthread_exit(0);
 }
 
--- src/simulation.c
+++ src/simulation.c
@@ -5,7 +5,6 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -19,11 +18,14 @@ time_t current_time;
 pid_t child_pid, wpid;
 int status;
 
-shared_memory_t *shared_memory;
+pthread_t sensor_thread, read_thread;
 
-void read_sensor_data(shared_memory_t* shared_memory);
+shared_memory_t shared_memory;
+volatile sig_atomic_t shutdown_requested = 0;
+
+void* read_sensor_data();
 void shutdown_handler(int sig);
-
+void cleanup();
 
 int main(int argc, char *argv[]) {
     if (argc != 4) {
@@ -31,13 +33,14 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    simulation_params_t params;
     // Parse the sensor mask from the hexadecimal string (e.g., 0x3F)
-    uint8_t sensors_enabled = (uint8_t)strtol(argv[1], NULL, 16); // Parse hex value
+    params.sensors_enabled = (uint8_t)strtol(argv[1], NULL, 16); // Parse hex value
 
     // Parse the sampling rate (in milliseconds)
-    int sampling_rate_ms = atoi(argv[2]);
+    params.sampling_rate_ms = atoi(argv[2]);
 
-    if (sampling_rate_ms <= 0) {
+    if (params.sampling_rate_ms <= 0) {
         fprintf(stderr, "Invalid sampling rate. It must be a positive integer.\n");
         exit(EXIT_FAILURE);
     }
@@ -48,82 +51,95 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Invalid runtime. It must be a positive integer.\n");
         exit(EXIT_FAILURE);
     }
+
     struct sigaction sa;
     sa.sa_handler = shutdown_handler;
 
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGTERM, &sa, NULL);
 
-    int pid;
 
     start_time = time(NULL);
 
+    shared_memory.sensors_updated = 0;
+    shared_memory.interrupt_sensors = 0;
 
-    int key = 1;
-    int id = shmget(key, sizeof(shared_memory_t), IPC_CREAT | 0644);
-    shared_memory = (shared_memory_t *) shmat(id, NULL, 0);
-    shared_memory->sensors_updated = 0;
-    shared_memory->interrupt_sensors = 0;
+    pthread_mutex_init(&shared_memory.mutex, NULL);
 
-    pthread_mutex_init(&shared_memory->mutex, NULL);
+    pthread_create(&sensor_thread, NULL, simulate_sensor_data, &params);
+    pthread_create(&read_thread, NULL, read_sensor_data, NULL);
 
-    if ((pid = fork()) > 0) {
-        child_pid = pid;
-        // child process: Simulate sensor data and write it to the pipe
-        simulate_sensor_data(sensors_enabled, shared_memory, sampling_rate_ms);
-        //kill(child_pid, SIGTERM);
-        //close(p[1]);
-    } else if (pid == 0){
-        current_time = time(NULL);
-        read_sensor_data(shared_memory);
-    } else {
-        perror("Fork failed");
-        exit(EXIT_FAILURE);
-    }
+    pthread_join(sensor_thread, NULL);
+    pthread_join(read_thread, NULL);
+    cleanup();
 
 
     return 0;
 }
 
-void read_sensor_data(shared_memory_t* shared_memory){
+//void read_sensor_data(shared_memory_t* shared_memory){
+void* read_sensor_data(){
 
     while (difftime(current_time, start_time) < runtime_sec) {
+
+        if (shutdown_requested) {
+            printf("Shutdown requested. Exiting read loop...\n");
+            break;
+        }
         time(&current_time);
-        pthread_mutex_lock(&shared_memory->mutex);
+        pthread_mutex_lock(&shared_memory.mutex);
 
         // critical zone
-        if (shared_memory->sensors_updated) {
+        if (shared_memory.sensors_updated) {
 
             for(int i = 0; i < NUM_SENSORS; i++) {
-		        printf("sensor %d: %0.2f\n", i + 1, shared_memory->sensor_buffer[i]);
+		        printf("sensor %d: %0.2f\n", i + 1, shared_memory.sensor_buffer[i]);
             }
 
-        shared_memory->sensors_updated = 0;
+        shared_memory.sensors_updated = 0;
         }
 
-        pthread_mutex_unlock(&shared_memory->mutex);
+        pthread_mutex_unlock(&shared_memory.mutex);
 
         // prevent spinning? doesn't seemed needed
         //usleep(500);
     }
 
-    pthread_mutex_lock(&shared_memory->mutex);
+    pthread_mutex_lock(&shared_memory.mutex);
 
-    shared_memory->interrupt_sensors = 1;
+    shared_memory.interrupt_sensors = 1;
 
-    pthread_mutex_unlock(&shared_memory->mutex);
+    pthread_mutex_unlock(&shared_memory.mutex);
 
+    pthread_exit(0);
 }
 
 void shutdown_handler(int sig) {
 
     printf("\nReceived signal %d\n pid: %d\n ppid: %d\nshutting down...\n", sig, getpid(), getppid());
-
-    pthread_mutex_unlock(&shared_memory->mutex);
-    pthread_mutex_destroy(&shared_memory->mutex);
-
-    if (child_pid != 0) {
-        printf("Parent: %d -- Terminating child process %d\n" , getppid(), child_pid);
+    if (sig == SIGTERM) {
+        cleanup();
+        exit(EXIT_SUCCESS);
     }
-    exit(EXIT_SUCCESS);
+
+    shutdown_requested = 1;
+}
+void cleanup() {
+    printf("Cleanup started. child pid: %d\n", child_pid);
+    //if (shared_memory != NULL) {
+        // Clean up shared memory and mutex
+        pthread_mutex_lock(&shared_memory.mutex);
+        pthread_mutex_unlock(&shared_memory.mutex);
+        pthread_mutex_destroy(&shared_memory.mutex);
+
+        //shmdt(shared_memory);  // Detach shared memory
+     //}
+
+     //if (child_pid != getpid() && child_pid != 0) {
+     //    printf("Parent: %d -- Terminating child process %d\n", getppid(), child_pid);
+     //    kill(child_pid, SIGTERM);  // Terminate child process if necessary
+     //    waitpid(child_pid, &status, 0);  // Wait for the child to terminate
+     //}
+
+        printf("Cleanup complete.\n");
 }
