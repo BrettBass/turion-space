fc73cdbc41c44a1bff16b2124bdf805d56b8abce
Brett Bass <bhbass@proton.me>
2024-11-24 16:52:49 -0800

    shared memory struct

--- include/sensor_simulator.h
+++ include/sensor_simulator.h
@@ -1,7 +1,10 @@
 #ifndef SENSOR_SIMULATOR_H
 #define SENSOR_SIMULATOR_H
 
+//#include <bits/pthreadtypes.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
 
 // Constants
 #define NUM_SENSORS 6
@@ -11,8 +14,15 @@
 // Extern declaration for the global sensor buffer
 extern float sensor_buffer[NUM_SENSORS];
 
+typedef struct {
+    float sensor_buffer [NUM_SENSORS];
+    bool interrupt_sensors;
+    bool sensors_updated;
+    pthread_mutex_t mutex;
+} shared_memory_t;
+
 float generate_sensor_data(void);
-void read_sensors(uint8_t sensors_enabled);
-void simulate_sensor_data(uint8_t sensors_enabled, int sampling_rate_ms, int p);
+void read_sensors(uint8_t sensors_enabled, shared_memory_t* shared_memory);
+void simulate_sensor_data(uint8_t sensors_enabled, shared_memory_t* shared_memory, int sampling_rate_ms);
 
 #endif // SENSOR_SIMULATOR_H
