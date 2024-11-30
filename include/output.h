167312da08bd5f372e193986d363a4d6a8ce43f1
Brett Bass <bhbass@proton.me>
2024-11-30 11:53:38 -0800

    formatting

--- include/output.h
+++ include/output.h
@@ -6,7 +6,7 @@
 
 #define MAX_BAR_LENGTH 50  // Max number of characters for the bar (you can adjust this)
 #define MAX_SENSOR_VALUE 100.0  // The max value to scale sensor data to
-                                //
-void print_avg(const float* averages, const int window_size);
+
+void print_avg(float average, const int window_size, int index);
 
 #endif // OUTPUT_H
