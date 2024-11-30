5737473848c0a9b17aac7638e2c02f5d213dd813
Brett Bass <bhbass@proton.me>
2024-11-30 11:53:08 -0800

    atomic interrupt value over mutex

--- include/moving_average.h
+++ include/moving_average.h
@@ -1,6 +1,7 @@
 #ifndef MOVING_AVERAGE_H
 #define MOVING_AVERAGE_H
 
+#include <sys/wait.h>
 #include <time.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -8,10 +9,14 @@
 #include "shared_memory.h"
 #include "output.h"
 
-void* moving_average(void* arg);
-void update_buffers(float** sensor_buffer, float* averages, int read_index, int window_size);
 
 extern shared_memory_t shared_memory;
+
+typedef struct {
+    bool buffer_full; // Buffer full flags for each sensor
+    int read_index;   // Read indices for each sensor
+    float average;   // Average values for each sensor
+} moving_average_t;
 
 typedef struct {
     int runtime_sec;
@@ -19,5 +24,8 @@ typedef struct {
     time_t start_time;
     float **sensor_buffer;
 } moving_average_params_t;
+
+void* moving_average(void* arg);
+void update_buffer(float** sensor_buffer, moving_average_t* moving_averages, int sensor_index, int read_index, int window_size);
 
 #endif // MOVING_AVERAGE_H
