19eb0ec8a1de199d964c4dc624ce2b2e12f87a13
Brett Bass <bhbass@proton.me>
2024-11-30 11:56:30 -0800

    individual sensor interrupts w/ timers

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdatomic.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,9 +10,9 @@
 #include <sys/wait.h>
 #include <sys/shm.h>
 
-#include "include/utils.h"
-#include "include/sensor_simulator.h"
-#include "include/moving_average.h"
+#include "../include/utils.h"
+#include "../include/sensor_simulator.h"
+#include "../include/moving_average.h"
 
 
 pthread_t sensor_thread, moving_average_thread;
@@ -32,12 +33,13 @@ int main(int argc, char *argv[]) {
     moving_average_params.sensor_buffer = sensor_buffer;
     moving_average_params.start_time = time(NULL);
 
-    shared_memory.sensors_updated = 0;
-    shared_memory.interrupt_sensors = 0;
+    shared_memory.sensors_update_mask = 0;
+    //shared_memory.interrupt_sensors = 0;
+    atomic_store(&shared_memory.interrupt_sensors, 0);
     pthread_mutex_init(&shared_memory.mutex, NULL);
 
-    pthread_create(&sensor_thread, NULL, simulate_sensor_data, &sensor_params);
     pthread_create(&moving_average_thread, NULL,moving_average, &moving_average_params);
+    pthread_create(&sensor_thread, NULL, simulate_sensor_data, &sensor_params);
 
     pthread_join(sensor_thread, NULL);
     pthread_join(moving_average_thread, NULL);
@@ -61,7 +63,7 @@ void cleanup() {
 }
 void parse_arguments(int argc, char* argv[], sensor_params_t* sensor_params, moving_average_params_t* ma_params) {
 
-    if (argc != 5) {
+    if (argc < 5) {
         fprintf(stderr, "Usage: %s <sensor_mask_hex> <sampling_rate_ms> <runtime_sec> <window size>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
@@ -70,24 +72,26 @@ void parse_arguments(int argc, char* argv[], sensor_params_t* sensor_params, mov
     sensor_params->sensors_enabled = (uint8_t)strtol(argv[1], NULL, 16); // Parse hex value
 
     // Parse the sampling rate (in milliseconds)
-    sensor_params->sampling_rate_ms = atoi(argv[2]);
+    int sampling_rate_ms;
+    int j = 0;
+    for (int i = 0; i < NUM_SENSORS; i++) {
+        if ( !(sensor_params->sensors_enabled & (1 << i)) ) continue;
 
-    if (sensor_params->sampling_rate_ms <= 0) {
-        fprintf(stderr, "Invalid sampling rate. It must be a positive integer.\n");
-        exit(EXIT_FAILURE);
+        sampling_rate_ms = atoi(argv[2 + j++]);
+
+        if (sampling_rate_ms <= 0)
+            die("Invalid sampling rate. It must be a positive integer.\n");
+
+        sensor_params->sampling_rates_ms[i] = sampling_rate_ms;
     }
 
     // Parse the runtime (in seconds)
-    ma_params->runtime_sec = atoi(argv[3]);
-    if (ma_params->runtime_sec <= 0) {
-        fprintf(stderr, "Invalid runtime. It must be a positive integer.\n");
-        exit(EXIT_FAILURE);
-    }
+    ma_params->runtime_sec = atoi(argv[3 + j - 1]);
+    if (ma_params->runtime_sec <= 0)
+        die("Invalid runtime. It must be a positive integer.\n");
 
-    ma_params->window_size = atoi(argv[4]);
+    ma_params->window_size = atoi(argv[4 + j - 1]);
 
-    if (ma_params->window_size <= 0) {
-        fprintf(stderr, "Invalid window siez. It must be a positive integer.\n");
-        exit(EXIT_FAILURE);
-    }
+    if (ma_params->window_size <= 0)
+        die("Invalid window siez. It must be a positive integer.\n");
 }
