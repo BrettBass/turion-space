79806e618a2bb1b143454b74a2bffe50eaab5b14
Brett Bass <bhbass@proton.me>
2024-11-30 11:54:42 -0800

    atomic usage updates, need to reformat input parser/msg

--- include/shared_memory.h
+++ include/shared_memory.h
@@ -1,16 +1,18 @@
 #ifndef SHARED_MEMORY_H
 #define SHARED_MEMORY_H
 
+#include <stdatomic.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <stdint.h>
 
 // Constants
 #define NUM_SENSORS 6
 
 typedef struct {
     float sensor_data [NUM_SENSORS];
-    bool interrupt_sensors;
-    bool sensors_updated;
+    atomic_bool interrupt_sensors;
+    int sensors_update_mask;
     pthread_mutex_t mutex;
 } shared_memory_t;
 
