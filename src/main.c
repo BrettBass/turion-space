639836375e45abd9a22cd5e3a690c4ec36f6aca6
Brett Bass <bhbass@proton.me>
2024-12-02 14:54:08 -0800

    inline comments

--- src/main.c
+++ src/main.c
@@ -18,13 +18,16 @@ int main(int argc, char *argv[]) {
     parse_arguments(argc, argv, &sensor_params, &moving_average_params, &runtime_sec);
     initialize_buffers(moving_average_params.sensor_weights, moving_average_params.window_size, sensor_params.sensors_enabled);
 
+    // initialize required values
     shared_memory.sensors_update_mask = 0; //set udpated sensors to NONE
     atomic_store(&shared_memory.shutdown, false);
     pthread_mutex_init(&shared_memory.mutex, NULL);
 
+    // create threads for simulation and calculations
     pthread_create(&moving_average_thread, NULL,moving_average, &moving_average_params);
     pthread_create(&sensor_simulation_thread, NULL, simulate_sensors, &sensor_params);
 
+    // begin shutdown once runtime is met
     while (difftime(time(NULL), start_time) < runtime_sec);
     atomic_store(&shared_memory.shutdown, true);
 
